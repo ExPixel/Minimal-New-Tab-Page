321328e337b825bc2109718a15192fa7e246235f
Adolph C <adolph.celestin@gmail.com>
2017-10-05 02:15:49 -0400

    Adds commands to package.json for building and packaging.

--- tools/package-extension.js
+++ tools/package-extension.js
@@ -28,10 +28,48 @@ function mkdirpAsync(dir, opts) {
     });
 }
 
+function readFileAsync(file) {
+    return new Promise((resolve, reject) => {
+        FileSystem.readFile(file, (err, data) => {
+            if (err) reject(err);
+            else resolve(data);
+        });
+    });
+}
+
+function writeFileAsync(file, data) {
+    return new Promise((resolve, reject) => {
+        FileSystem.writeFile(file, data, (err) => {
+            if (err) reject(err);
+            else resolve();
+        });
+    });
+}
+
+/**
+ * This makes sure that the version of the manifest file matches the version of the package.json.
+ */
+async function manifestFix(version) {
+    const manifestString = (await readFileAsync("dist/manifest.json")).toString("utf8");
+    const manifest = JSON.parse(manifestString);
+
+    if (!manifest.version) {
+        console.warn("[WARNING] NO MANIFEST VERSION FOUND. ADDING.");
+    } else if (manifest.version != version) {
+        console.warn("[WARNING] MANIFEST VERSION (%s) DOES NOT MATCH PACKAGE VERSION (%s). CHANGING.", manifest.version, version);
+    }
+
+    manifest.version = version;
+    const newManifestString = JSON.stringify(manifest, null, 2);
+    await writeFileAsync("dist/manifest.json", newManifestString);
+    console.log("Wrote new manifest.json file.");
+}
+
 async function main(args) {
     const package = require("../package.json");
-    const output = args[0].replace("[version]", package.version);
+    await manifestFix(package.version);
 
+    const output = args[0].replace("[version]", package.version);
     const outputDirName = Path.dirname(output);
     if (outputDirName && outputDirName.length > 0) {
         await mkdirpAsync(outputDirName);
